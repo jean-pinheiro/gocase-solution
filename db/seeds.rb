f08de1fdbefe6c124d4a66fb872d51806de80976
Jean Pinheiro <jean.pinheiro@fortaleza.ce.gov.br>
2018-11-03 08:50:51 -0300

    populate and migrate db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Order.create( reference: 'BR102030' , purchase_channel: 'SiteBR', client_name: 'Rogerio Lima',
+ address: 'Rua Padre Valdevino, 2475 - Aldeota, Fortaleza - CE, 60135-041',
+ delivery_service:'SEDEX', total_value: '123.30',
+ line_items:'[{sku: case-my-best-friend, model: iPhone X, case type: Rose Leather}, {sku: powebank-sunshine, capacity: 10000mah}]',
+ status: 'ready' )
+
+Order.create( reference: 'DE102030' , purchase_channel: 'SiteDE', client_name: 'Johan Schultz',
+ address: 'Altenberger Str. 13 50668 Köln, Deutschland',
+ delivery_service:'FEDEX', total_value: '123.30',
+ line_items:'[{sku: earphone-standard, color: white}, {sku: powebank-sunshine, capacity: 10000mah}]',
+ status: 'ready' )
+
+Order.create( reference: 'NL102030' , purchase_channel: 'SiteNL', client_name: 'Matjis Snowden',
+ address: 'Alberdingk Thijmstraat 8II 1054 AJ, Amsterdam, Netherlands',
+ delivery_service:'NEDEX', total_value: '123.30',
+ line_items:'[{sku: case-my-best-friend, model: iPhone X, case type: Rose Leather}, {sku: earphone-standard, color: white}]',
+ status: 'ready' )
+
+Order.create( reference: 'BR204050' , purchase_channel: 'SiteBR', client_name: 'Diogo Silva',
+ address: 'Rua Padre Mororo, 475 - Centro, Fortaleza - CE 60015-220',
+ delivery_service:'SEDEX', total_value: '123.30',
+ line_items:'[{sku: case-my-best-friend, model: iPhone X, case type: Rose Leather}, {sku: earphone-standard, color: white}, {sku: powebank-sunshine, capacity: 10000mah}]',
+ status: 'ready' )
+
+Order.create( reference: 'BR502030' , purchase_channel: 'SiteBR', client_name: 'Mario Sergio',
+ address: 'R. Ana Bilhar, 572 - Meireles, Fortaleza - CE, 60160-110',
+ delivery_service:'SEDEX', total_value: '123.30',
+ line_items:'[{sku: powebank-sunshine, capacity: 10000mah}, {sku: earphone-standard, color: white}]',
+ status: 'ready' )
