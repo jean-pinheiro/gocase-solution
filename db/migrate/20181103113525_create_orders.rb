24d0276d87b06c80ba3cdb6b1d55061a66fe59b5
Jean Pinheiro <jean.pinheiro@fortaleza.ce.gov.br>
2018-11-03 08:35:51 -0300

    Add model Order

---
+++ db/migrate/20181103113525_create_orders.rb
@@ -0,0 +1,16 @@
+class CreateOrders < ActiveRecord::Migration[5.0]
+  def change
+    create_table :orders do |t|
+      t.string :reference
+      t.string :purchase_channel
+      t.string :client_name
+      t.text :addres
+      t.string :delivery_service
+      t.float :total_value
+      t.text :line_items
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
