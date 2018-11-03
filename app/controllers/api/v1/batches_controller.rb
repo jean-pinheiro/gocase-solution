e4f7178b85ec4903c935c17a0766fa04f9a6ca88
Jean Pinheiro <jean.pinheiro@fortaleza.ce.gov.br>
2018-11-03 10:30:11 -0300

    Controllers Indentation"

--- app/controllers/api/v1/batches_controller.rb
+++ app/controllers/api/v1/batches_controller.rb
@@ -7,64 +7,51 @@ module Api
       end 
 
       def create
-        batch = Batch.new(batch_params)
-
-        orders = Order.where(purchase_channel: params[:purchase_channel])
-
-          orders.each do |o|
-            batch.orders << o 
-          end  
-
-        if batch.save
-          render json: {status: 'SUCCESS', message:'Saved Batch', data:batch},status: :ok
-        else
-          render json: {status: 'ERROR', message:'Batch not saved', data:batch.errors},status: :unprocessable_entity
-        end
+	        batch = Batch.new(batch_params)
+	        orders = Order.where(purchase_channel: params[:purchase_channel])
+	        orders.each do |o|
+	            batch.orders << o 
+	        end  
+	        if batch.save
+	          render json: {status: 'SUCCESS', message:'Saved Batch', data:batch},status: :ok
+	        else
+	          render json: {status: 'ERROR', message:'Batch not saved', data:batch.errors},status: :unprocessable_entity
+	        end
       end
 
       def production
-        batch = Batch.where(reference: params[:reference]).first
-                
-        orders = batch.orders
-
-        orders.each { |o| 
-          o.status = "production"
-          o.save()
-        }
-
-        render json: {status: 'SUCCESS', message:'Batch sent to Production', data: orders},status: :ok 
+	        batch = Batch.where(reference: params[:reference]).first
+	        orders = batch.orders
+	        orders.each { |o| 
+	          o.status = "production"
+	          o.save()
+	        }
+	        render json: {status: 'SUCCESS', message:'Batch sent to Production', data: orders},status: :ok 
       end
 
       def close
-        batch = Batch.where(reference: params[:reference]).first
-        orders = batch.orders
-
-        orders.each { |o|
-          o.status ="sent"
-          o.delivery_service = params[:delivery_service]
-          o.save()
-        }
-      
-        render json: {status: 'SUCCESS', message:'Batch Closed', data: orders},status: :ok 
+	        batch = Batch.where(reference: params[:reference]).first
+	        orders = batch.orders
+	        orders.each { |o|
+	          o.status ="sent"
+	          o.delivery_service = params[:delivery_service]
+	          o.save()
+	        }   
+	        render json: {status: 'SUCCESS', message:'Batch Closed', data: orders},status: :ok 
       end
 
       private
-
-        def batch_params
-
-          y = Date.today.year
-          m = Date.today.month
-          idl = Batch.last
-          id = (idl == nil ? 1 : idl.id+1)
-          
-          ref = "#{y}#{m}-#{id}"
-          {
-            :reference => ref, 
-            :purchase_channel => params[:purchase_channel], 
-            
-          }
-              
-        end
+	        def batch_params
+	          y = Date.today.year
+	          m = Date.today.month
+	          idl = Batch.last
+	          id = (idl == nil ? 1 : idl.id+1)          
+	          ref = "#{y}#{m}-#{id}"
+	          {
+	            :reference => ref, 
+	            :purchase_channel => params[:purchase_channel]            
+	          }
+	        end
 
     end
   end
--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -7,28 +7,22 @@ module Api
       end 
 
       def create
-        order = Order.new(order_params)
-
-        if order.save
-          render json: {status: 'SUCCESS', message:'Saved order', data:order},status: :ok
-        else
-          render json: {status: 'ERROR', message:'Order not saved', data:order.errors},status: :unprocessable_entity
-        end
+          order = Order.new(order_params)
+          if order.save
+            render json: {status: 'SUCCESS', message:'Saved order', data:order},status: :ok
+          else
+            render json: {status: 'ERROR', message:'Order not saved', data:order.errors},status: :unprocessable_entity
+          end
       end
 
       def show_status
-        order = Order.where(reference: params[:reference]).first
-
-        render json: {status: 'SUCCESS', message:'Status of Order', data:order.status},status: :ok
+          order = Order.where(reference: params[:reference]).first
+          render json: {status: 'SUCCESS', message:'Status of Order', data:order.status},status: :ok
       end
 
-       def financial_report
-        
-        num_or = Order.select('COUNT(purchase_channel) as qty, SUM(total_value) as total, purchase_channel')
-          .group(:purchase_channel)
-               
-        render json: {status: 'SUCCESS', message:'Financial Report by Purchase Channel', data:num_or},status: :ok
-      
+       def financial_report       
+          num_or = Order.select('COUNT(purchase_channel) as qty, SUM(total_value) as total, purchase_channel').group(:purchase_channel)               
+          render json: {status: 'SUCCESS', message:'Financial Report by Purchase Channel', data:num_or},status: :ok
       end
 
       def list_per_channel
@@ -39,13 +33,11 @@ module Api
             data: orders
           },status: :ok
       end
-
       
       private
-
-      def order_params
-        params.permit(:reference, :purchase_channel, :client_name, :address, :delivery_service, :total_value, :line_items, :status)
-      end
+        def order_params
+          params.permit(:reference, :purchase_channel, :client_name, :address, :delivery_service, :total_value, :line_items, :status)
+        end
     end
   end
 end
