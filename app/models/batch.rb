4f437f25c38a827c1bd84a2befbdc328d8c9a8b2
Jean Pinheiro <jean.pinheiro@fortaleza.ce.gov.br>
2018-11-04 09:59:52 -0300

    Add Model Validations

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,3 +1,6 @@
 class Batch < ApplicationRecord
 	has_and_belongs_to_many :orders
+
+	validates :referece, uniqueness: true, on: :create
+	validates :purchase_channel, presence: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,10 @@
 class Order < ApplicationRecord
 	has_and_belongs_to_many :batches
+
+	  validates :referece, uniqueness: true, on: :create
+      validates :purchase_channel, presence: true, on: :create
+      validates :client_name, presence: true
+      validates :address, presence: true
+      validates :delivery_service, allow_blank: true
+      validates :status, inclusion: { in: %w(ready production sent) }
 end
