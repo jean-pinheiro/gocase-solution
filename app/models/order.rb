6a98db548793924208b390b986f10914a1b38cff
Jean Pinheiro <jean.pinheiro@fortaleza.ce.gov.br>
2018-11-04 10:25:52 -0300

    config rspec tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,6 @@ class Order < ApplicationRecord
       validates :purchase_channel, presence: true, on: :create
       validates :client_name, presence: true
       validates :address, presence: true
-      validates :delivery_service, allow_blank: true
+      validates :delivery_service, presence: true
       validates :status, inclusion: { in: %w(ready production sent) }
 end
